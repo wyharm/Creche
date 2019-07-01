7a50c3b00cb09d0db8ecb8e5efc9895fb16c3b05
Armougom <y.armougom@gmail.com>
2019-07-02 01:44:09 +0200

    animations animaux sur footer

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,11 @@
+/*AOS.init({
+  duration: 1200,
+})*/
+
+
+
+
+
 $(document).ready(function(){
   $(".menu").hide();
     $(".hamburger").click(function(){
@@ -13,19 +21,20 @@ $(document).ready(function(){
 
 
 
-/*AOS.init({
-  duration: 1200,
-})*/
+function stopAnimate() {clearInterval(time);
+}
 
-var time;
+let time;
 
 function animateScript() {
   var    position = 300;
   const  interval = 100;
+  const  diff = 300;
   time = setInterval ( () => {
-    document.getElementById("image").style.backgroundPosition =`-${position}px 0px`; 
+    document.getElementById("image").style.backgroundPosition =`-${position}px 0px`;
+    document.getElementById("image1").style.backgroundPosition =`-${position}px 0px`;
     if (position < 4500)
-      { position = position + 300;}
+      { position = position + diff;}
     else
 { position = 300; }
     }
@@ -34,6 +43,27 @@ function animateScript() {
 
 
 
+
+
+window.addEventListener('scroll',() => {
+
+  let scrolled = window.scrollY;
+  console.log(scrolled);
+  
+  if (scrolled >= 5161) {
+  document.getElementById("image").style.top='72%';
+  document.getElementById("image1").style.top='72%';
+  animateScript();
+  
+  }
+  else {
+  document.getElementById("image1").style.top='100%';
+  document.getElementById("image").style.top='100%';
+  stopAnimate();
+  }
+  
+  
+  });
 
 
 
