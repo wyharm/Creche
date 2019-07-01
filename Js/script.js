f52bfbffa8b91b370dde7abb17ea4fe4a360e93b
Armougom <y.armougom@gmail.com>
2019-07-01 23:23:32 +0200

    fonctionne sur Chrome

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,24 @@
-AOS.init({
+$(document).ready(function(){
+  $(".menu").hide();
+    $(".hamburger").click(function(){
+        $(".menu").fadeIn(500);
+    });
+      $(".closeBtn").click(function(){
+        $(".menu").fadeOut(500);
+    });
+});
+
+
+
+
+
+
+/*AOS.init({
   duration: 1200,
-})
+})*/
 
 var time;
+
 function animateScript() {
   var    position = 300;
   const  interval = 100;
@@ -16,28 +32,10 @@ function animateScript() {
 , interval );}
 
 
-window.addEventListener('scroll',() => {
-
-let scrolled = window.scrollY;
-console.log(scrolled);
- 
-
-if (scrolled >= 2000) {
-
-  document.getElementById("image").style.visibility='visible';
-  
-
-  
-}
-else {
-  document.getElementById("image").style.visibility='hidden';
- 
- 
-}
 
 
 
-});
+
 
 
 
