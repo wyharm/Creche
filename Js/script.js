8b5a7ad70e393f476c9f450ff0fc8b57d878fd4e
Armougom <y.armougom@gmail.com>
2019-07-03 09:44:07 +0200

    toggle navbar

--- Js/script.js
+++ Js/script.js
@@ -22,6 +22,19 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 } 
 
+$(function(){ 
+  var navMain = $(".navbar-collapse"); // avoid dependency on #id
+  // "a:not([data-toggle])" - to avoid issues caused
+  // when you have dropdown inside navbar
+  navMain.on("click", "a:not([data-toggle])", null, function () {
+      navMain.collapse('hide');
+  });
+});
+
+
+
+
+
 
 function stopAnimate() {clearInterval(time);
 }
